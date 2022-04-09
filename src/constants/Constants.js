4d8670486a12e95f266a4600ae0e7133f683da72
rvren <rv.renjith22@gmail.com>
2022-04-10 01:38:03 +0530

    Updated font sizes

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -2,25 +2,25 @@ export const SECTION_CONSTANTS = [
   {
     title: "Hello1",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Quam quisque id diam vel quam elementum pulvinar etiam non.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     bgcolor: "linear-gradient(128deg,#40afff,#3f61ff)",
   },
   {
     title: "Hello2",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Quam quisque id diam vel quam elementum pulvinar etiam non.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     bgcolor: "linear-gradient(128deg,#ff9a3f,#ff4b40)",
   },
   {
     title: "Hello3",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Quam quisque id diam vel quam elementum pulvinar etiam non.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     bgcolor: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)",
   },
   {
     title: "Hello4",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Quam quisque id diam vel quam elementum pulvinar etiam non.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     bgcolor: "linear-gradient(128deg,#fc40ff,#543fff)",
   },
 ];
